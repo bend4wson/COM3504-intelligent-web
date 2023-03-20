c7ba1776dd46ac0569d23f402da9ea28b9d2fa19
bend4wson <ben.dawson735@gmail.com>
2023-03-20 23:52:53 +0000

    Added & linked registration and add sighting pages

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bird Watching Page' });
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Registration Page' });
+});
+
+router.get('/add_sighting', function(req, res, next) {
+  res.render('addSighting', { title: 'Add a new Sighting' });
+});
+
 module.exports = router;
