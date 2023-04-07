003f36ce42231aa58380b3c01b6e73bd37a1b682
bend4wson <ben.dawson735@gmail.com>
2023-04-07 14:49:34 +0100

    Refactoring code, getting rid of unecessary endpoints

--- routes/sightings.js
+++ routes/sightings.js
@@ -3,43 +3,14 @@ const router = express.Router();
 const Sighting = require('../databases/sightings');
 const Users = require("../databases/users");
 
-// const multer = require('multer');
-// const upload = multer(); // We will use memory storage for the file upload
-
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
-
-// Example endpoint to create a new sighting
-// router.post('/create', async (req, res) => {
-//     try {
-//         const newSighting = new Sighting(req.body);
-//         await newSighting.save();
-//         res.status(201).json({ message: 'Sighting added successfully', data: newSighting });
-//     } catch (error) {
-//         res.status(400).json({ message: 'Error adding sighting', error });
-//     }
-// });
-
-// router.post('/add_sighting', async (req, res) => {
-//     try {
-//         console.log("****")
-//         const newSighting = new Sighting(req.body);
-//         await newSighting.save();
-//         console.log("Sighting added successfully");
-//         res.redirect('/');
-//     } catch (error) {
-//         console.log("!!!!")
-//         console.error("Error adding sighting", error);
-//         res.status(400).json({ message: 'Error adding sighting', error });
-//     }
-// });
 
 router.get('/', async (req, res) => {
     console.log("!!");
     res.render('index', { title: 'Bird Watching Page' });
 });
 
-// router.post('/add_sighting', async (req, res) => {
 router.post('/add_sighting', upload.single('picture'), async (req, res) => {
     try {
         console.log("1");
