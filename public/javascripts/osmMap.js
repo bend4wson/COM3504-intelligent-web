ea850b0543176d6ed68163f4187096b46efbb7c6
bend4wson <87946100+bend4wson@users.noreply.github.com>
2023-05-11 16:58:51 +0100

    Map location can now be chosen when creating a sighting

--- public/javascripts/osmMap.js
+++ public/javascripts/osmMap.js
@@ -1,8 +1,28 @@
-function initMap(lat, lng) {
-    console.log("THE VALUES ARE: " + lat + " " + lng)
-    // var lat = sightingLatitude;
-    // var lng = sightingLongitude;
+// function initMap(lat, lng) {
+//     console.log("THE VALUES ARE: " + lat + " " + lng)
+//     // var lat = sightingLatitude;
+//     // var lng = sightingLongitude;
+//
+//     // Initialize the map
+//     var map = L.map('map').setView([lat, lng], 13);
+//
+//     // Set the OSM tile layer
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution:
+//             '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//         maxZoom: 18,
+//     }).addTo(map);
+//
+//     // Add a marker at the sighting location
+//     var marker = L.marker([lat, lng]).addTo(map);
+// }
+//
+// // Call the initMap function to display the map when the page loads
+// window.addEventListener('DOMContentLoaded', initMap);
 
+var marker;  // Initialize marker variable globally
+
+function initMapWithDraggableMarker(lat, lng) {
     // Initialize the map
     var map = L.map('map').setView([lat, lng], 13);
 
@@ -13,9 +33,34 @@ function initMap(lat, lng) {
         maxZoom: 18,
     }).addTo(map);
 
-    // Add a marker at the sighting location
-    var marker = L.marker([lat, lng]).addTo(map);
+    // Add a draggable marker at the sighting location
+    marker = L.marker([lat, lng], {draggable: 'true'}).addTo(map);
+
+    // Update form inputs whenever the marker is dragged
+    marker.on('dragend', function(event) {
+        var position = marker.getLatLng();
+        document.getElementById('lat').value = position.lat;
+        document.getElementById('lng').value = position.lng;
+        document.getElementById('userLat').value = position.lat;
+        document.getElementById('userLng').value = position.lng;
+    });
 }
 
-// Call the initMap function to display the map when the page loads
-window.addEventListener('DOMContentLoaded', initMap);
+function initMapWithStaticMarker(lat, lng) {
+    // Initialize the map
+    var map = L.map('map').setView([lat, lng], 13);
+
+    // Set the OSM tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+    }).addTo(map);
+
+    // Add a static marker at the sighting location
+    marker = L.marker([lat, lng]).addTo(map);
+}
+
+
+window.initMapWithDraggableMarker = initMapWithDraggableMarker;
+window.initMapWithStaticMarker = initMapWithStaticMarker;
